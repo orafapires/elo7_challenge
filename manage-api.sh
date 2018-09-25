e5fc5137e2ab074bf9e797c79ce98a3d111bc27e
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2018-09-25 16:08:21 -0300

    Merge start API script with utils

--- manage-api.sh
+++ manage-api.sh
@@ -21,18 +21,18 @@ MONGO="mongo"
 API="elo7-challenge"
 
 command_exists(){
-    type "$1" &> /dev/null;
+    type "$1"
 }
 
 error_cmd_not_exists(){
     if [ "$?" -eq 1 ]; then
-        color r "O $element é o mínimo que precisamos... bora instalar?"
+        color r "O $element é o mínimo que precisamos para executar a API :)?"
     fi
 }
 
 check_dependencies(){
     for element in "${dependencies[@]}"; do
-        command_exists $element
+        command_exists "$element"
         error_cmd_not_exists
     done
 }
@@ -55,7 +55,7 @@ start_api_with_compose(){
 
 health_check_api(){
     while [ "$RESPONSE" != 200 ]; do
-        RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $BASE_URL`
+        RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" $BASE_URL)
         if [ "$RESPONSE" -eq 200 ]; then
             color b "API em funcionamento"
         else
@@ -65,7 +65,7 @@ health_check_api(){
 }
 
 get_state_running(){
-    PS=`docker ps --format '{{.Names}}' --filter 'name='$1''`
+    PS=$(docker ps --format '{{.Names}}' --filter 'name='"$1"'')
     if [ -z "$PS" ]; then
             color y "$1 não está em execução"
             return 1
@@ -88,20 +88,16 @@ start_api_without_compose(){
 remove_container(){
     CONTAINER="$1"
     color b "Removendo $CONTAINER"
-    docker kill $CONTAINER &> /dev/null;
-    docker rm $CONTAINER &> /dev/null;
+    docker kill "$CONTAINER" &> /dev/null;
+    docker rm "$CONTAINER" &> /dev/null;
     color g "$CONTAINER removido"
 }
 
 stop_api_without_compose(){
     get_state_running $MONGO
-        if [ "$?" -eq 0 ]; then
-            remove_container $MONGO
-        fi
+    remove_container $MONGO
     get_state_running $API
-        if [ "$?" -eq 0 ]; then
-            remove_container $API
-        fi
+    remove_container $API
 }
 
 check_dependencies
