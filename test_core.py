d13aaec45ff26216ade93d316c6e8a6ca6cf289d
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2018-09-27 19:25:50 -0300

    Implementing tests for route post

--- test_core.py
+++ test_core.py
@@ -21,14 +21,20 @@ class TestElo7Challenge(unittest.TestCase):
 
     def test_post_error(self):
         item = {"name": "some_item"}
+        response = '{"message":"Bad request parameters!","ok":false}\n'
         self.response = app.post('http://localhost:5000/deploy-time',
                                  data=json.dumps(item),
                                  content_type='application/json')
         self.assertEqual(self.response.status_code, 400)
+        self.assertEqual(response, self.response.data.decode('utf-8'))
 
     def test_post_sucess(self):
         item = { "component" : "teste", "version" : "2.0", "accountable" : "eu", "status" : "teste"}
+        response = '{"message":"Deploy data stored successfully.","ok":true}\n'
         self.response = app.post('http://localhost:5000/deploy-time',
                                  data=json.dumps(item),
                                  content_type='application/json')
         self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(response, self.response.data.decode('utf-8'))
+
+    
