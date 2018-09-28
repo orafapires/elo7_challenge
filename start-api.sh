168dff7063802955d8f13a4bff9987640a3a604e
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2018-09-28 11:35:58 -0300

    Adjust open project directory

--- start-api.sh
+++ start-api.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/orafapires/elo7_challenge.git && cd ./elo7_challenge
+git clone --depth=1 https://github.com/orafapires/elo7_challenge.git
+cd ./elo7_challenge
 source ./manage-api.sh
 start_api_with_compose
