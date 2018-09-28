de93169b058ecf5ead9738441db6b85a4748d99e
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2018-09-28 11:32:53 -0300

    Adjust directory to clone project

--- start-api.sh
+++ start-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/orafapires/elo7_challenge.git
+git clone --depth=1 https://github.com/orafapires/elo7_challenge.git && cd ./elo7_challenge
 source ./manage-api.sh
 start_api_with_compose
