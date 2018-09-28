a60d46a2fc1a2e01d9001634f0fd3b8586894865
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2018-09-28 11:55:25 -0300

    Improve script to start API

--- start-api.sh
+++ start-api.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/orafapires/elo7_challenge.git
-cd ./elo7_challenge
-source ./manage-api.sh
-start_api_with_compose
+# Check if git have instaled
+main(){
+command -v git >/dev/null 2>&1 || {
+    echo "Git não instalado :("
+    exit 1
+  }
+}
+
+# Start API
+start_api(){
+    git clone --depth=1 https://github.com/orafapires/elo7_challenge.git
+    cd ./elo7_challenge || {
+        echo "Diretório do projeto não encontrado :("
+        exit 1
+    }
+    source ./manage-api.sh
+    start_api_with_compose
+}
+
+main
+start_api
